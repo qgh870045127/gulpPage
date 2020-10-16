89ccab30f7e6ac21289af457062498578e8e6795
PC-20180905FEGK\Administrator <QGH78666@163.COM>
2020-10-16 14:53:09 +0800

    添加注释

--- cluster.js
+++ cluster.js
@@ -1,6 +1,7 @@
 var express = require('express')
 var app = express()
 
+// 映射打包的资源
 app.use('/', express.static(__dirname + '/dist/html'))
 app.use('/js', express.static(__dirname + '/dist/js'))
 app.use('/css', express.static(__dirname + '/dist/css'))
